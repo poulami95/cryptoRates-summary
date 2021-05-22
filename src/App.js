709bbca285a3ea907a1ff7a9b7f58e6958e64b76
Poulami Saha <39046609+poulami95@users.noreply.github.com>
2021-05-23 01:00:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import Header from "./components/contents/Header/Header";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    document.title = "CC Exchange rates";
+  }, []);
   return (
     <div className="App">
       <Header />
